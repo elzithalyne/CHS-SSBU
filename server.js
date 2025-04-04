f0f0445ec7894d2cd5672a48b8c53d31369c47a7
Elzi <loromoli07@outlook.com>
2025-04-04 01:21:00 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -7,11 +7,20 @@ const PORT = 3000;
 // Middleware to parse JSON requests
 app.use(express.json());
 
+
 // Serve static files from the project folder
 app.use(express.static(__dirname));
 
 // Path to the JSON file
 const dataFilePath = path.join(__dirname, 'competitors.json');
+
+function capitalizeName(name) {
+    return name
+        .toLowerCase()
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ')
+}
 
 // Endpoint to check in a competitor
 app.post('/checkin', (req, res) => {
@@ -32,8 +41,10 @@ app.post('/checkin', (req, res) => {
         let competitor = competitors.find(c => c.name.toLowerCase() === name.toLowerCase());
 
         if (competitor) {
+            const formattedName = capitalizeName(name);
             if (competitor.checkedIn) {
-                return res.send(`${name} is already checked in.`);
+                console.log(`${formattedNameame} is already checked in`)
+                return res.send(`${formattedName} is already checked in.`);
             } else {
                 competitor.checkedIn = true;
                 fs.writeFile(dataFilePath, JSON.stringify(competitors, null, 2), err => {
@@ -41,12 +52,15 @@ app.post('/checkin', (req, res) => {
                         console.error(err);
                         return res.status(500).send('Error updating data file.');
                     }
-                    res.send(`Checked In, Welcome ${name}`);
+                    res.send(`Checked In, Welcome ${formattedName}! Your bracket spot is ${competitor.bracketSpot}!`);
+                    console.log(`Checked In: ${formattedName}, ${competitor.bracketSpot}`)
                 });
             }
         } else {
             res.send('This is for competitor check-ins only. See host for spectator/substitute sign-in.');
+            
         }
+    
     });
 });
 
