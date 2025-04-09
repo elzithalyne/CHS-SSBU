96d264c99977533ca47c1fc19cd9ec5266c25798
Elzi <loromoli07@outlook.com>
2025-04-09 10:54:49 -0600

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,7 +1,21 @@
 function redirect() { 
-  window.open ("https://competitor-check-in.onrender.com/prizes.html")
+    window.open ("http://localhost:3000/prizes.html")
+    console.log("Prizes Window Opened")
 }
 
+let maintenance = document.getElementById('maint')
+
 function redirectMSB() {
-  console.log("redirectMSB() deployed")
+
+    maintenance.style.display = "block";
+    maintenance.style.color = "red";
+    maintenance.style.border = "2px solid red";
+    maintenance.textContent = "MSB is not yet connected. See Mr. Richman in room R2 for entry purchase."
+
+}
+
+function formGGL() {
+
+    window.open('https://forms.gle/fPmTsKVdAtbpwbTJA')
+
 }
